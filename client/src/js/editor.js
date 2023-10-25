5fbabff2b87cf01a2735a550f2ab5434a1281f36
Alex Purfield <alexpurfield@gmail.com>
2023-10-24 23:12:35 -0600

    finally fixed the buuugsgit add .

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -6,13 +6,13 @@ export default class {
   constructor() {
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
 
     this.editor = CodeMirror(document.querySelector('#main'), {
-      value: '',
+      value: '', // Ensure that the initial value is a string
       mode: 'javascript',
       theme: 'monokai',
       lineNumbers: true,
@@ -22,18 +22,19 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // When the editor is ready, set the value to whatever is stored in IndexedDB.
+    // Fall back to localStorage if nothing is stored in IndexedDB, and if neither is available, set the value to header.
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      // Ensure that data is a string before setting it in the editor.
+      this.editor.setValue(data.toString() || localData || header.toString());
     });
 
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
